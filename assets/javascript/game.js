fe312950d7419c8a6ab539d5cf7b56baae8f73e1
Spiritbr8ker <stephen.oveson@gmail.com>
2019-05-25 13:35:52 -0600

    Fully functional game, styling to font

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,73 +22,78 @@ function initGame() {
     guessesRemaining = 5;
     lettersGuessed = [];
     updateDOM()
-  }
-  initGame()
-  function wordHasBeenGuessed(){
-    for(let i=0; i<currentWord.length; i++){
+}
+initGame()
+function wordHasBeenGuessed() {
+    for (let i = 0; i < currentWord.length; i++) {
         if (!letterHasBeenGuessed(currentWord[i]))
             return false;
     }
     return true
-  }
-  function assignCurrentWord() {
-    // const index = Math.floor(Math.random() * ((possibleWords.length -1) - 0 + 1)) + 0;
+}
+function assignCurrentWord() {
+    // const index = Math.floor(Math.random() * ((possibleWords.length - 1) - 0 + 1)) + 0;
     const index = 6;
     currentWord = possibleWords[index]
-  }
-  function setEventListeners(){
-    document.onkeyup = function(press){
-    if (press.which < 48 || press.which > 90) 
-        return;
-    if (press.altKey || press.metaKey || press.ctrlKey) 
-        return;
-    if (lettersGuessed.includes(press.key)) 
-        return;
-    lettersGuessed.push(press.key.toUpperCase())
-    if(wordHasBeenGuessed()){
-        wins++
-        document.getElementById('wins-count').textContent = wins
-        alert('You win!')
-        initGame()
-      }
-      shouldGuessesGoDown(press.key)
-      console.log(currentWord, lettersGuessed, guessesRemaining)
-      updateDOM()
-      checkIfUserLost()
+}
+function setEventListeners() {
+    document.onkeyup = function (press) {
+        if (press.which < 48 || press.which > 90)
+            return;
+        if (press.altKey || press.metaKey || press.ctrlKey)
+            return;
+        if (letterHasBeenGuessed(press.key))
+            return;
+        lettersGuessed.push(press.key.toUpperCase())
+        if (wordHasBeenGuessed()) {
+            wins++
+            document.getElementById('wins-count').textContent = wins
+            initGame()
+        }
+        shouldGuessesGoDown(press.key)
+        console.log(currentWord, lettersGuessed, guessesRemaining)
+        updateDOM()
+        checkIfUserLost()
     }
-  }
-  function checkIfUserLost(){
-    if(guessesRemaining <= 0){
-      alert('you lost')
-      initGame();
+}
+function checkIfUserLost() {
+    if (guessesRemaining <= 0) {
+        wins = 0;
+        alert('Your Prince Will Never Come\n\nWins Reset!')
+        initGame();
     }
-  }
-  function shouldGuessesGoDown(lettersGuessed){
-    if(!currentWord.includes(lettersGuessed)){
-      guessesRemaining = guessesRemaining - 1
+}
+function shouldGuessesGoDown(lettersGuessed) {
+    if (!currentWord.includes(lettersGuessed)) {
+        guessesRemaining = guessesRemaining - 1
     }
-  }
-  function updateDOM(){
+}
+function updateDOM() {
     document.getElementById("guesses-remaining").textContent = guessesRemaining
     document.getElementById("letters-guessed").textContent = lettersGuessed
     showLettersOrDashes()
-  }
-  function showLettersOrDashes() {
+}
+function showLettersOrDashes() {
     let displayWord = ''
-    for(let i=0; i<currentWord.length; i++){
-      if(lettersGuessed.includes(currentWord[i])){
-        displayWord = displayWord + currentWord[i] + ' '
-      } else {
-        displayWord = displayWord + '_' + ' '
-      }
+    for (let i = 0; i < currentWord.length; i++) {
+        let letter = currentWord[i];
+        if (letter === ' ') {
+            displayWord += '\n'
+        }
+        else if (letterHasBeenGuessed(letter)) {
+            displayWord += letter + ' '
+        } else {
+            displayWord += '_' + ' '
+        }
     }
-    document.getElementById('display-word').textContent = displayWord
-  }
-  
-  function letterHasBeenGuessed(letter){
-        return letter === ' ' || lettersGuessed.includes(letter.toUpperCase())
-  }
 
-  
+    document.getElementById('display-word').textContent = displayWord
+}
+
+function letterHasBeenGuessed(letter) {
+    return letter === ' ' || lettersGuessed.includes(letter.toUpperCase())
+}
+
+
 
 
